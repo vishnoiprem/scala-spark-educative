12fc45fbb94d7b73daf43276fe4e7a65b30e906a
Prem.Vishnoi <prem.vishnoi@LAZ-SG-L-M-1409.local>
2022-08-05 20:15:10 +0800

    added

---
+++ src/main/scala/SparkSql/SparkHiveDB.scala
@@ -0,0 +1,62 @@
+
+
+package SparkSql
+
+
+
+
+// Imports
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object SparkHiveDB  {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[1]")
+      .appName("SparkByExamples")
+      .getOrCreate()
+    import spark.implicits._
+
+    val movies = spark.read.format("csv")
+      .option("header","true")
+      .option("inferSchema","true")
+      .load("src/main/resources/data/BollywoodMovieDetail.csv")
+    print(movies.columns)
+
+    // Print the result
+    movies.createOrReplaceTempView("tmp_movie_Vw")
+    // Find all movies released after 2010 and labelled as average, above average or below average
+
+    val sql_select ="""select * FROM tmp_movie_Vw where releaseYear>2010"""
+    spark.sql(sql_select).show(3)
+
+    //    spark.sql("SELECT title FROM tmp_movie_Vw WHERE releaseYear > 2010 ORDER BY title desc").show(3)
+    val sql= """
+                SELECT title,
+                hitFlop,
+                CASE
+                  WHEN hitFlop < 5 THEN 'below average'
+                  WHEN hitFlop = 5 THEN 'average'
+                  WHEN hitFlop > 5 THEN 'above average' END AS MovieRating
+                FROM tmp_movie_Vw WHERE releaseYear > 2010 ORDER BY hitFlop desc, title desc
+                """
+    //
+    val df = spark.sql(sql)
+    df.show(10)
+    //
+    //    # Equivalent query using DatFrame API
+    (movies.where($"releaseYear" > 2010)
+      .withColumn("MovieRating",
+        when($"hitFlop" === 5,"average" )
+          .when($"hitFlop">5,"above average")
+          .otherwise("below average"))
+      .select($"title", $"MovieRating")
+      .sort(desc("title"))
+      .show(10))
+
+
+
+  }
+}
--- src/main/scala/SparkSql/sparkSqlExmple.scala
+++ src/main/scala/SparkSql/sparkSqlExmple.scala
@@ -23,36 +23,36 @@ object sparkSqlExmple  {
       .load("src/main/resources/data/BollywoodMovieDetail.csv")
       print(movies.columns)
 
-    // Print the result
-    movies.createOrReplaceTempView("tmp_movie_Vw")
-    // Find all movies released after 2010 and labelled as average, above average or below average
+    spark.sql("CREATE DATABASE if not exists spark_course")
+    spark.sql("USE spark_course")
+    spark.sql("SHOW TABLES").show(5, false)
 
-    val sql_select ="""select * FROM tmp_movie_Vw where releaseYear>2010"""
-    spark.sql(sql_select).show(3)
+    spark.sql("CREATE TABLE if not exists movieShortDetail(imdbID String, title String)")
 
-//    spark.sql("SELECT title FROM tmp_movie_Vw WHERE releaseYear > 2010 ORDER BY title desc").show(3)
-    val sql= """
-                SELECT title,
-                hitFlop,
-                CASE
-                  WHEN hitFlop < 5 THEN 'below average'
-                  WHEN hitFlop = 5 THEN 'average'
-                  WHEN hitFlop > 5 THEN 'above average' END AS MovieRating
-                FROM tmp_movie_Vw WHERE releaseYear > 2010 ORDER BY hitFlop desc, title desc
-                """
-//
-    val df = spark.sql(sql)
-    df.show(10)
-//
-//    # Equivalent query using DatFrame API
-    (movies.where($"releaseYear" > 2010)
-      .withColumn("MovieRating",
-        when($"hitFlop" === 5,"average" )
-          .when($"hitFlop">5,"above average")
-          .otherwise("below average"))
-      .select($"title", $"MovieRating")
-      .sort(desc("title"))
-      .show(10))
+    spark.sql("SHOW TABLES").show(5, false)
+    spark.conf.set("spark.sql.legacy.allowCreatingManagedTableUsingNonemptyLocation","true")
+//    movies.write.option("mode","overwrite").saveAsTable("movieShortDetailUsingDataFrame")
+    spark.sql("CREATE TABLE if not exists movieShortDetailUnmanaged (imdbID STRING, title STRING) USING csv OPTIONS (PATH 'src/main/resources/data/BollywoodMovieDetail.csv')")
+    movies.write.option("path","src/main/resources/data/shortMovieDetail.csv").option("mode","overwrite").saveAsTable("movieShortDetailUsingDataFrameUnmanaged")
+
+
+    spark.catalog.listTables().show(5, false)
+    spark.catalog.listColumns("movieshortdetail").show(10, false)
+    spark.catalog.listDatabases().show(10, false)
+    movies.write.saveAsTable("movies")
+    spark.sql("CREATE OR REPLACE TEMP VIEW high_rated_movies AS SELECT title FROM movies WHERE hitFlop > 7")
+    spark.catalog.listTables().show(3)
+    spark.catalog.listTables().show(5,false)
+    spark.sql("CREATE OR REPLACE GLOBAL TEMP VIEW high_rated_movies_global AS SELECT title FROM movies WHERE hitFlop > 7")
+    spark.sql("SELECT * FROM global_temp.high_rated_movies_global").show(3, false)
+    spark.catalog.listDatabases().show(3)
+
+    spark.catalog.listTables().show(3)
+
+    spark.catalog.listColumns("movies").show(3)
+
+
+
 
 
 
